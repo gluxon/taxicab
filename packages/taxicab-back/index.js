c80d43b99511b974f1e8c7b99c519773990dde91
Brandon Cheng <brandon.cheng@protonmail.com>
2018-01-19 15:57:56 -0500

    Rearrange module imports to top of file

--- packages/taxicab-back/index.js
+++ packages/taxicab-back/index.js
@@ -1,59 +1,49 @@
 require('dotenv').config()
 
 const Koa = require('koa')
+const Router = require('koa-router')
+const crypto = require('crypto')
+const db = require('./models')
+const session = require('koa-session')
+const createCASMiddleware = require('koa-cas-auth')
+const csrf = require('koa-csrf-header')
+
+const requireLogin = require('./middleware/requireLogin')
+const controllers = require('./controllers')
+
 const app = new Koa()
 
-const crypto = require('crypto')
 app.keys = [(process.env.APP_KEY || crypto.randomBytes(256))]
-
 app.proxy = process.env.NODE_ENV === 'development'
 
-const db = require('./models')
 db.sequelize.sync() // Create tables that do not exist yet.
 app.context.db = db
 
-const session = require('koa-session')
 app.use(session(app))
 
-const Router = require('koa-router')
 const router = new Router()
 router.prefix('/api')
 
-const createCASMiddleware = require('koa-cas-auth')
 const cas = createCASMiddleware({ server: 'https://login.uconn.edu' })
 const credentialRequestor = cas.credentialRequestor({
   renew: true,
   setUser: (ctx, user) => {
-    ctx.cookies.set('user', user, {
-      signed: true,
-      // Setting httpOnly allows the JS frontend to read the currently logged
-      // in user.
-      httpOnly: false
-    })
-    ctx.db.user.findOrCreate({
-      where: { netid: user }
-    })
+    ctx.cookies.set('user', user, { signed: true, httpOnly: false })
+    ctx.db.user.findOrCreate({ where: { netid: user } })
   },
   getUser: ctx => ctx.cookies.get('user', { signed: true })
 })
 
-const requireLogin = require('./middleware/requireLogin')
-
 router.use(credentialRequestor)
 router.use(requireLogin)
 
-const csrf = require('koa-csrf-header')
 router.use(csrf({
   setToken: (token, ctx) => {
     ctx.session.csrfToken = token
-    ctx.cookies.set('csrf-token', token, {
-      signed: true,
-      httpOnly: false
-    })
+    ctx.cookies.set('csrf-token', token, { signed: true, httpOnly: false })
   }
 }))
 
-const controllers = require('./controllers')
 for (const controller of Object.values(controllers)) {
   router.use(controller.routes(), controller.allowedMethods())
 }
